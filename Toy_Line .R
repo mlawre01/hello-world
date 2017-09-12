ba95f87b75a91b27ee0fca7ae06cfe9e40bdfc7b
Mackenzie Lawrence <mackenzie_lawrence111@hotmail.com>
2017-09-12 10:39:51 -0700

    toy_line commit

---
+++ Toy_Line .R
@@ -0,0 +1,15 @@
+# 3
+a <- 2
+b <- -3
+sig_sq <- 0.5
+x <- runif(40)
+y <- a + b * x + rnorm(40, sd = sqrt(sig_sq))
+(avg_x <- mean(x))
+
+# 4
+write(avg_x, "avg_x.txt")
+plot(x, y)
+abline(a, b, col = "purple")
+
+# 5
+dev.print(pdf, "toy_line_plot.pdf")
